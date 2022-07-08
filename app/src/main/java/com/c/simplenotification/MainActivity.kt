49201080281372cc46f0ea12a0219e4c97047c2f
jaypithavaelsner <jaypithavaelsner@gmail.com>
2022-07-08 23:27:39 +0530

    Pending Intent and Large Image Added in Notification icon.

--- app/src/main/java/com/c/simplenotification/MainActivity.kt
+++ app/src/main/java/com/c/simplenotification/MainActivity.kt
@@ -2,8 +2,10 @@ package com.c.simplenotification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -47,10 +49,28 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun sendNotification(){
+
+        //Pending Intent Added Fot Notification Click
+
+        val intent=Intent(this,MainActivity::class.java).apply {
+            flags=Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+
+        val pendingIntent:PendingIntent=PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_IMMUTABLE)
+
+
+        //Large Image Added in Notification
+
+        val bitmap=BitmapFactory.decodeResource(applicationContext.resources,R.drawable.spider_man)
+        val bitmapLargeIcon=BitmapFactory.decodeResource(applicationContext.resources,R.drawable.iron_man)
+
         val builder=NotificationCompat.Builder(this,channelID)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
             .setContentTitle("Example Title")
             .setContentText("Example Description")
+            .setLargeIcon(bitmapLargeIcon)
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+            .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         with(NotificationManagerCompat.from(this)){
